957ab48e148a13c0a3a880c3653a47005866df63
Luis Posadas <luisposadasp96@outlook.es>
2017-08-24 10:31:08 -0500

    se agrego el nombre dy autor del libro

---
+++ Libro.java
@@ -0,0 +1,12 @@
+
+public class  Libro
+{
+    private String titulo;
+    private String autor;
+    public Libro(String tituloi,String autori)
+    {
+        titulo = tituloi;
+        autor = autori;
+    }
+    
+}
