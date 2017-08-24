3361c589447fe115050f96dd7e0902800aeaa79d
Luis Posadas <luisposadasp96@outlook.es>
2017-08-24 10:42:24 -0500

    se agregaron metodos  dime autor y dime titulo

--- Libro.java
+++ Libro.java
@@ -8,5 +8,13 @@ public class  Libro
         titulo = tituloi;
         autor = autori;
     }
+    public String dimeAutor()
+    {
+        return autor;
+    }
+    public String dimeTitulo()
+    {
+        return titulo;
+    }
     
 }
