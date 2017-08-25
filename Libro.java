37048db970adc3db942d4f0af18f378722d52937
Luis Posadas <luisposadasp96@outlook.es>
2017-08-25 10:23:18 -0500

    se modifico numero de referencia

--- Libro.java
+++ Libro.java
@@ -1,11 +1,14 @@
 
 public class  Libro
 {
+  
     private String titulo;
     private String autor;
     private int paginas;
+    private String numeroDeReferencia;
     public Libro(String tituloi,String autori, int paginasi)
     {
+        numeroDeReferencia = "";
         titulo = tituloi;
         autor = autori;
         paginas = paginasi;
@@ -24,6 +27,7 @@ public class  Libro
     }
     public String dimeDetalles()
     {
+      
         String cadResultado = "";
         cadResultado = cadResultado + "Autor";
         cadResultado = cadResultado + ":";
@@ -32,7 +36,35 @@ public class  Libro
         cadResultado = cadResultado + ":";
         cadResultado = cadResultado + titulo;
         cadResultado = cadResultado + "Paginas";
+        cadResultado = cadResultado + ":";
         cadResultado = cadResultado + paginas;
+        
+  
+        if(numeroDeReferencia != ""){
+            cadResultado = cadResultado + "numeroDeReferencia";
+            cadResultado = cadResultado + ":";
+            cadResultado = cadResultado + numeroDeReferencia;
+  
+        }
+        else
+        {
+            cadResultado = cadResultado + "numeroDeReferencia";
+            cadResultado = cadResultado + ":";
+            cadResultado = cadResultado + "zzz";
+        }
         return cadResultado;
+        
     }
+    public void cambiaNumRef(String numRefUsr)
+    {
+        numeroDeReferencia =  numRefUsr;
+    }
+     public String dimeNumRef()
+     {
+         return numeroDeReferencia;
+        
+     }
+     
+    
+    
 }
